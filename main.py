82ead07669ce3d226dfab19d55a412750668f245
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-05-01 17:02:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Lists Exercise
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# How to pick a random name, using a random index based on the number of items in the list
+# How to get the random name and the output "Name is going to buy the meal today!"
+print(names)
+# Names = ["Lenovo", "MSI", "ASUS", "RAZER", "ACER"]
