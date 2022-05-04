241d3c2c6af40931239dbc5869cfeef157644156
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-05-04 16:47:01 +0000

    I have used the "random.choice" module to solve the exercise.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import random
 # Lists Exercise
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # How to pick a random name, using a random index based on the number of items in the list
 # How to get the random name and the output "Name is going to buy the meal today!"
 print(names)
+print(random.choice(names), "will be paying the bill for the meal today =D !")
 # Names = ["Lenovo", "MSI", "ASUS", "RAZER", "ACER"]
