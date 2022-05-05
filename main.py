d0aceb5f5ac8d65fc19e7e76f0916826982f0cd7
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-05-05 21:05:17 +0000

    Just need to figure out how to print the random list item as a string!

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # How to pick a random name, using a random index based on the number of items in the list
 # How to get the random name and the output "Name is going to buy the meal today!"
+#Solition 1
 print(names)
-print(random.choice(names), "will be paying the bill for the meal today =D !")
-# Names = ["Lenovo", "MSI", "ASUS", "RAZER", "ACER"]
+num_items = len(names)
+all_names = random.randint(0, num_items -1)
+text_names = str(all_names)
+print(text_names + "is going to pay for the meal today!")
+
+#Solution 2
+# print(names)
+# print(random.choice(names), "will be paying the bill for the meal today =D !")
+# Names = ["John", "David", "James", "Max", "Zebra"]
